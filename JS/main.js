fbe247cbf9c74580328f484acec261708b282810
ahmed <ahmedmijdo2@gmail.com>
2024-12-17 08:42:33 +0200

    add back to top button

--- JS/main.js
+++ JS/main.js
@@ -5,19 +5,19 @@ let secondP = document.getElementById("second");
 let theEnd = document.getElementById("end");
 
 
-btnMore.addEventListener("click" , function() {
+btnMore.addEventListener("click" , function () {
     firstP.style.cssText = "display:block";
-    btnMore.addEventListener("click" , function() {
+    btnMore.addEventListener("click" , function () {
         secondP.style.cssText = "display:block";
         btnMore.addEventListener("click" , function() {
             theEnd.style.cssText = "display:block";
             btnMore.style.cssText = "display:none";
             btnLess.style.cssText = "display:block";
-            btnLess.addEventListener("click",function(){
+            btnLess.addEventListener("click",function () {
                 theEnd.style.cssText = "display:none";
-                btnLess.addEventListener("click",function(){
+                btnLess.addEventListener("click",function () {
                     secondP.style.cssText = "display:none";
-                    btnLess.addEventListener("click",function(){
+                    btnLess.addEventListener("click",function () {
                         firstP.style.cssText = "display:none";
                         btnLess.style.cssText = "display:none";
                         btnMore.style.cssText = "display:block";
@@ -28,7 +28,24 @@ btnMore.addEventListener("click" , function() {
     });
 });
 
+// Back to Top Btn
 
+
+// Back to Top Btn
+let backToTop = document.getElementById("back-top");
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+};
+backToTop.addEventListener("click", function () {
+    window.scrollTo ({
+        top:0,
+        behavior:"smooth",
+    });
+});
 // print the current year 
 
 let myDate = document.getElementById("years");
